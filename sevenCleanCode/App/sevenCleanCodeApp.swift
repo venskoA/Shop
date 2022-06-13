ff43b3958ae6f7493692a5db619d202e365c4f17
venskoA <venskoandrey@gmail.com>
2022-06-13 23:09:01 +0300

    create Start project

---
+++ sevenCleanCode/App/sevenCleanCodeApp.swift
@@ -0,0 +1,20 @@
+//
+//  sevenCleanCodeApp.swift
+//  sevenCleanCode
+//
+//  Created by Andrey Vensko on 13.06.22.
+//
+
+import SwiftUI
+
+@main
+struct sevenCleanCodeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
