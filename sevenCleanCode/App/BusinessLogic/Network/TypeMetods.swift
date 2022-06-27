cb21c10248c2c04ced4b1f9355f82fccc657a3d7
venskoA <venskoandrey@gmail.com>
2022-06-27 20:11:27 +0300

    create json request

---
+++ sevenCleanCode/App/BusinessLogic/Network/TypeMetods.swift
@@ -0,0 +1,15 @@
+//
+//  TypeMetods.swift
+//  sevenCleanCode
+//
+//  Created by Andrey Vensko on 23.06.22.
+//
+
+import Foundation
+
+enum TypeMetod: String {
+    case login = "/GeekBrainsTutorial/online-store-api/master/responses/login.json"
+    case lodout = "/GeekBrainsTutorial/online-store-api/master/responses/logout.json"
+    case reist = "/GeekBrainsTutorial/online-store-api/master/responses/registerUser.json"
+    case change = "/GeekBrainsTutorial/online-store-api/master/responses/changeUserData.json"
+}
