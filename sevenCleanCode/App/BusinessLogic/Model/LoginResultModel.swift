3700d31badc3d661c9cc414f960369d984b697da
venskoA <venskoandrey@gmail.com>
2022-06-27 20:10:39 +0300

    create network model

---
+++ sevenCleanCode/App/BusinessLogic/Model/LoginResultModel.swift
@@ -0,0 +1,27 @@
+//
+//  LoginResultModel.swift
+//  sevenCleanCode
+//
+//  Created by Andrey Vensko on 23.06.22.
+//
+
+import Foundation
+
+struct LoginResult: Codable {
+    let result: Int
+    let user: User
+}
+
+struct User: Codable {
+    let id: Int
+    var login: String
+    let name: String
+    let lastname: String
+
+    enum CodingKeys: String, CodingKey {
+        case id = "id_user"
+        case login = "user_login"
+        case name = "user_name"
+        case lastname = "user_lastname"
+    }
+}
