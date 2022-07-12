43a8ae30e31fe480b01a6fbcc859fcc6f0ce29a6
venskoA <venskoandrey@gmail.com>
2022-07-12 21:55:21 +0300

    crate model for goods

---
+++ sevenCleanCode/Entity/GoodsModel/GoodsModel.swift
@@ -0,0 +1,28 @@
+//
+//  GoodsModel.swift
+//  sevenCleanCode
+//
+//  Created by Andrey Vensko on 5.07.22.
+//
+
+import Foundation
+
+struct GoodsModel: Codable, Identifiable {
+    var id: UUID = UUID()
+
+    var idProduct: Int
+    var productName: String
+    var price: Int
+    var quantity: Int?
+    var image: String?
+    var description: String?
+
+    enum CodingKeys: String, CodingKey {
+        case idProduct = "id_product"
+        case productName = "product_name"
+        case price
+        case quantity
+        case image
+        case description
+    }
+}
