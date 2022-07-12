b7f85826293221618d0613555e3fba52b71acb91
venskoA <venskoandrey@gmail.com>
2022-07-12 22:29:09 +0300

    create goods view

---
+++ sevenCleanCode/Flows/Goods/Goods.swift
@@ -0,0 +1,31 @@
+//
+//  Goods.swift
+//  sevenCleanCode
+//
+//  Created by Andrey Vensko on 9.07.22.
+//
+
+import SwiftUI
+
+struct Goods: View {
+    
+    @ObservedObject var presenter = GoodsPresenter()
+
+    var body: some View {
+            List(presenter.goods) { good in
+                NavigationLink {
+                    GoodDescription(good: good,
+                                    presenter: GoodDescriptionPresenter(idGood: good.idProduct))
+                } label: {
+                    GoodCellList(good: good)
+                        .navigationTitle("Shop")
+                }
+            }
+    }
+}
+
+struct Goods_Previews: PreviewProvider {
+    static var previews: some View {
+        Goods()
+    }
+}
