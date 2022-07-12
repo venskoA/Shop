3306c3d8952170cca8b72c32987115b90ca1ff69
venskoA <venskoandrey@gmail.com>
2022-07-12 22:34:14 +0300

    no message

--- sevenCleanCode/SevenCleanCodeApp.swift
+++ sevenCleanCode/SevenCleanCodeApp.swift
@@ -10,12 +10,12 @@ import SwiftUI
 @main
 struct SevenCleanCodeApp: App {
     let persistenceController = PersistenceController.shared
-    var dataModel = EnterModel(id: 123, userName: "aa", password: "123", email: "ooo@gmail.com", gender: .mail, creditCards: "98980-99090-900", bio: "---")
+    var dataModel = UserData(id: 123, userName: "aa", password: "123", email: "ooo@gmail.com", gender: .mail, creditCards: "98980-99090-900", bio: "---")
 
     var body: some Scene {
         WindowGroup {
             let presenter = StartPresenter()
-            StartView(dataModel: dataModel, presenter: presenter)
+            StartView()
 //            ContentView()
 //                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
